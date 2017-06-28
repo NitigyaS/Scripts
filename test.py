3ff9bcfd010691050a53c9116ea8b15794d30387
Nitigya <nitigyasharma@hotmail.com>
2017-06-28 17:16:50 +0530

    Google Score Checker

---
+++ test.py
@@ -0,0 +1,31 @@
+import requests
+import json
+from email.mime.text import MIMEText
+from subprocess import Popen, PIPE
+
+
+
+def sendmail():
+    message = "Google Page Test Says your Score is " + str(score)
+    msg = MIMEText(message)
+    msg["From"] = "me@moglix.com"
+    msg["To"] = "nitigya.sharma@gmail.com"
+    msg["Subject"] = "This is the subject."
+    p = Popen(["/usr/sbin/sendmail", "-t", "-oi"], stdin=PIPE)
+    p.communicate(msg.as_string())
+
+#Fetch Data from the URL
+r = requests.get("https://www.googleapis.com/pagespeedonline/v2/runPagespeed?url=http%3A%2F%2Fwww.moglix.com")
+text=""
+if r.status_code == 200:
+   text=json.loads(r.text)
+
+
+score = text["ruleGroups"]["SPEED"]["score"]
+if score < 80:
+   print "Your Score is less :" , score
+   sendmail()
+   print "Mail Send"
+else:
+   print "Your Score is OK" , score
+
